5a5af7353e60f2d6f23d4127ca163d988f94e19d
KuR0uSaGi <teenoy92545@gmail.com>
2021-09-16 18:11:11 +0700

    sort

--- 63010852_Lab01_2.py
+++ 63010852_Lab01_2.py
@@ -1,7 +1,7 @@
-print("*** multiplication or sum ***")
+# print("*** multiplication or sum ***")
 
-num1, num2 =[int(i)for i in input("Enter num1 num2 : ").split()]
+# num1, num2 =[int(i)for i in input("Enter num1 num2 : ").split()]
 
-if (num1*num2)>1000:
-    print("The result is",num1+num2)
-else: print("The result is",num1*num2)
+# if (num1*num2)>1000:
+#     print("The result is",num1+num2)
+# else: print("The result is",num1*num2)
