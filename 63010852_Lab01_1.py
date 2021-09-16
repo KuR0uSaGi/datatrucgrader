f961c4639e17d59d6dd53543772282e7600f4ed4
KuR0uSaGi <teenoy92545@gmail.com>
2021-09-16 18:10:06 +0700

    sort

--- 63010852_Lab01_1.py
+++ 63010852_Lab01_1.py
@@ -1,10 +1,11 @@
-print("*** Rabbit & Turtle ***")
+# print("*** Rabbit & Turtle ***")
 
-d, Vr, Vt, Vf = [float(i) for i in input("Enter Input : ").split()]
+# d, Vr, Vt, Vf = [float(i) for i in input("Enter Input : ").split()]
 
 
-Vt_r = Vt - Vr
+# Vt_r = Vt - Vr
 
-D = Vf * (d/Vt_r)
+# D = Vf * (d/Vt_r)
 
-print("{:.2f}".format(float(D)))
+# print("{:.2f}".format(float(D)))
+# print(heloo)
